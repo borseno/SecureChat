da38fedf7063a64db1eafeda130ac991b50d15ac
markvrc <markvirchenko@gmail.com>
2020-04-11 22:00:29 +0300

    added key generation and its sample usage

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    Console.OutputEncoding <- System.Text.Encoding.Unicode
+    let str = CryptoAlgorithms.KeyGenerator.KeyGenerator.getValues 50
+    printfn "%s" str
+    0 // return an integer exit code
---
+++ CryptoAlgorithms/KeyGenerator.fs
@@ -0,0 +1,22 @@
+﻿namespace CryptoAlgorithms.KeyGenerator
+
+module RandomOrg =
+    open FSharp.Data
+    open System
+
+    let formatRequest = sprintf "https://www.random.org/integers/?num=%i&min=%i&max=%i&col=1&base=10&format=plain&rnd=new"
+
+    let next amount (min, max) = 
+        let nums = Http.RequestString(formatRequest amount min max).Split("\n")
+        Seq.take (nums.Length - 1) nums 
+        |> Seq.map (fun i -> Int32.Parse(i))
+
+module KeyGenerator =
+
+    open System
+    
+    let getValues count =
+        RandomOrg.next count (1040, 1103)
+        |> Seq.map (fun i -> char i) |> Seq.toArray |> String
+
+
