c5bee89ff5b4067b6111e1de2502abce3bc44188
markvrc <markvirchenko@gmail.com>
2020-04-11 18:35:00 +0300

    Added vernam cypher algorithm to the codebase

---
+++ CryptoAlgorithms/OneTimePad.fs
@@ -0,0 +1,8 @@
+﻿namespace CryptoAlgorithms
+
+module OneTimePad =
+    let validateInput key text = Seq.length key = Seq.length text
+
+    let encrypt (key:#seq<char>) (text:#seq<char>) = 
+        Seq.zip key text |> 
+        Seq.map (fun (k, t) -> (int k) ^^^ (int t) |> char)
