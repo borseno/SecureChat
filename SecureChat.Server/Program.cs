61c7657cc4ae97674f09bdc09c080f7c6e61a35d
Марія Гриньова <mariia.hrynova@nure.ua>
2020-04-12 11:55:12 +0300

    Trying to connect to server

--- SecureChat.Server/Program.cs
+++ SecureChat.Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -16,6 +17,18 @@ namespace SecureChat.Server
             CreateHostBuilder(args).Build().Run();
         }
 
+        public class ChatHub : Hub
+        {
+            public async Task Send(string message, string userName)
+            {
+                await Clients.All.SendAsync("Receive", message, userName);
+            }
+        }
+
+        public static void SendMessage(string message, string public_key)
+        {
+
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
