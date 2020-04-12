d0aa0008f43a01e666be0b92fa8a8700825bfcb3
markvrc <markvirchenko@gmail.com>
2020-04-12 12:02:57 +0300

    added richer functionality to console key-generating project

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -2,9 +2,28 @@
 
 open System
 
+let (|Int|_|) (str:string) =
+    match System.Int32.TryParse str with
+    | true,int -> Some int
+    | _ -> None
+
+let printKey length = 
+        let str = CryptoAlgorithms.KeyGenerator.KeyGenerator.getValues length
+        printfn "%s" str
+
 [<EntryPoint>]
 let main argv =
     Console.OutputEncoding <- System.Text.Encoding.Unicode
-    let str = CryptoAlgorithms.KeyGenerator.KeyGenerator.getValues 50
-    printfn "%s" str
+    
+    let invalidInput = true
+    while invalidInput do
+        printfn "enter a length of key to generate"
+        let input = Console.ReadLine()
+        match input with
+        | Int num -> 
+            printKey (Int32.Parse input)
+            printfn "-----------------------"
+        | _ -> 
+            printfn "error, try again"
+
     0 // return an integer exit code
