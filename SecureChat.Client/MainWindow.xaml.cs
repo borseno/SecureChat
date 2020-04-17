5e3fe04e68df3185b5093267dae4f9c225e7f200
Марія Гриньова <mariia.hrynova@nure.ua>
2020-04-17 14:25:56 +0300

    almost ready

--- SecureChat.Client/MainWindow.xaml.cs
+++ SecureChat.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,55 @@ namespace SecureChat.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        HubConnection connection;
         public MainWindow()
         {
             InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:54847/")
+                .Build();
+
+            StartConnection();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+
+        private void GetNewVernamKey()
+        {
+
+
+
+
+            //PublicKey.Text = newKey;
+        }
+
+        private async void StartConnection()
+        {
+            await connection.StartAsync();
+        }
+        
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", ClientMessage.Text, PublicKey.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong... Please, try again");
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            //PublicKey.Text = "";
+            ClientMessage.Text = "";
         }
     }
 }
